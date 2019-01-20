573d4dd81b21eb48ab6986d137f3ef0240fae010
overoptimus <1040570917@qq.com>
2019-01-20 23:47:15 +0800

    2019.1.21

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('sunck is a good man!')
+
+
+def detail(request, num, num2):
+    return HttpResponse('detail-%s-%s' %(num,num2))
