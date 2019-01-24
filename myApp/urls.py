41e19ccc8e57f50ad366e0766c1f586a07dbdb5e
overoptimus <1040570917@qq.com>
2019-01-24 12:51:44 +0800

    2019.1.24

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('<int:num>/<int:num2>/',views.detail),
+    path('<int:num>/',views.detail),
+    path('grades/',views.grades),
+    path('students/', views.students),
+    path('grades/<int:id>/', views.gradeStudents),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Students,Grades
+
 
 def index(request):
     return HttpResponse('sunck is a good man!')
@@ -9,3 +11,21 @@ def index(request):
 
 def detail(request, num, num2):
     return HttpResponse('detail-%s-%s' %(num,num2))
+
+def grades(request):
+    #去模型里取数据
+    gradesList = Grades.objects.all()
+    #将数据传递给模板，模板再进行渲染页面，将渲染好的页面返回浏览器
+    return render(request, 'myApp/grades.html', {'grades': gradesList})
+
+
+def students(request):
+    studentsList = Students.objects.all()
+    return render(request, 'myApp/students.html', {'students': studentsList})
+
+
+def gradeStudents(request, id):
+    grade = Grades.objects.get(pk=id)
+    studentsList = grade.students_set.all()
+    return render(request, 'myApp/gradeStudents.html', {'students': studentsList,   \
+    'grade': grade})
